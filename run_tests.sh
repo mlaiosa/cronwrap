e31f0b1b58c20b988bf5cee2b2b0c4cc1a423e57
Mike Laiosa <mike@laiosa.org>
2011-01-08 21:01:52 -0800

    Improve the tester's output a bit

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+NUM_PASS=0
+NUM_FAIL=0
 
 for f in tests/*; do
 	if [ -x $f ] ;then
 		if [ ! -e $f.out ] ;then
 			echo $f.out missing.
 		else
-			($f | diff -q $f.out - > /dev/null) || echo $f failed.
+			if ($f | diff -q $f.out - > /dev/null) ;then
+				NUM_PASS=`expr $NUM_PASS + 1`
+			else
+				NUM_FAIL=`expr $NUM_FAIL + 1`
+				echo $f Failed.
+			fi
 		fi
 	fi
 done
+echo Passed: $NUM_PASS
+echo Failed: $NUM_FAIL
+exit $NUM_FAIL

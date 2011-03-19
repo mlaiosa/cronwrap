4a02cbec143565a76128571078312fac97f4578d
Mike Laiosa <mike@laiosa.org>
2011-03-18 23:05:35 -0700

    Add --save-output, so that the output can be (unconditionally) saved

--- main.c
+++ main.c
@@ -16,18 +16,70 @@ Copyright 2011 Mike Laiosa <mike@laiosa.org>.  Licensed under the GPLv2.
 #define UNUSED_VAR
 #endif
 
+static char *output_file = NULL;
 static char *tempfile = NULL;
 static void cleanup_tempfile(void) {
 	if (tempfile)
 		unlink(tempfile);
 }
 
+struct argument_description {
+	const char *name; char **dest;
+};
+
+/* Returns the position of the first positional argument. */
+static int parse_arguments(const struct argument_description *desc, int argc, char **argv) {
+	int i;
+	int arg;
+	int parsed;
+	for (arg = 1; arg < argc; ++arg) {
+		/* Everything following -- is positional. */
+		if (strcmp(argv[arg], "--") == 0)
+			return arg+1;
+		parsed = 0;
+		/* Go through and see if its one of our arguments. */
+		for (i = 0; !parsed && desc[i].name; ++i) {
+			const int len = strlen(desc[i].name);
+			if (strncmp(argv[arg], desc[i].name, len) == 0) {
+				if (argv[arg][len] == 0) {
+					++arg;
+					if (arg < argc)
+						*desc[i].dest = argv[arg];
+					else {
+						printf("%s requries an argument\n", desc[i].name);
+						exit(EXIT_FAILURE);
+					}
+					parsed = 1;
+				}
+				else if (argv[arg][len] == '=') {
+					*desc[i].dest = argv[arg] + len + 1;
+					parsed = 1;
+				}
+			}
+		}
+		/* If this wasn't one of our options, then its the first positional argument. */
+		if (!parsed)
+			return arg;
+	}
+	return arg;
+}
+
 int main(int argc, char **argv) {
 	pid_t child;
 	int log;
+	int first_program_arg;
+	static const struct argument_description arg_desc[] = {
+		{"--save-output", &output_file},
+		{NULL}
+	};
+	first_program_arg = parse_arguments(arg_desc, argc, argv);
+	if (first_program_arg >= argc) {
+		printf("At least one argument required\n");
+		return 1;
+	}
 
-	/* Create our temp file */
-	{
+	/* Create a temp file if we need one. */
+	if (output_file == NULL) {
 		const char *template = "/cronwrap.XXXXXX";
 		const char *tmpdir = getenv("TMPDIR");
 		if (tmpdir == NULL) tmpdir = "/tmp";
@@ -46,6 +98,17 @@ int main(int argc, char **argv) {
 			return 1;
 		}
 		atexit(cleanup_tempfile);
+		output_file = tempfile;
+	}
+	else {
+		/* TODO: There ought to be command line options to allow unlinking
+		 * rather than truncating, and to allow appending. Also, how to choose
+		 * a mode? Should I do 0666 and let umask handle it? */
+		log = open(output_file, O_WRONLY|O_TRUNC|O_CREAT, 0666);
+		if (log < 0) {
+			printf("Unable to open %s\n", output_file);
+			return 1;
+		}
 	}
 
 	child = fork();
@@ -61,7 +124,7 @@ int main(int argc, char **argv) {
 		dup2(log, 1);
 		dup2(log, 2);
 		close(log);
-		execvp(argv[1], argv+1);
+		execvp(argv[first_program_arg], argv+first_program_arg);
 		printf("Unable to launch %s\n", argv[1]);
 		return EXIT_FAILURE;
 	}
@@ -78,7 +141,7 @@ int main(int argc, char **argv) {
 		if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
 			exit(EXIT_SUCCESS);
 		}
-		log = open(tempfile, O_RDONLY, 0);
+		log = open(output_file, O_RDONLY, 0);
 		while ((bytes_read = read(log, buf, sizeof buf)) > 0) {
 			/* There's nothing we can really do if the write fails,
 			   but simply casting the result to void doesn't seem

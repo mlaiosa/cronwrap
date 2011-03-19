a786bb66c64105272c43a3a1ed393fb30dca30c0
Mike Laiosa <mike@laiosa.org>
2011-03-18 23:07:02 -0700

    Give the tester a progress bar

--- run_tests.sh
+++ run_tests.sh
@@ -3,21 +3,37 @@
 NUM_PASS=0
 NUM_FAIL=0
 
+INCOMPLETE=""
+ERRORS=""
+
 for f in tests/*; do
 	if [ -x $f ] ;then
+		INCOMPLETE="-$INCOMPLETE"
+	fi
+done
+INCOMPLETE="${INCOMPLETE}\r"
+echo -ne $INCOMPLETE
+
+for f in tests/*; do
+	if [ -x $f ] ;then
+		echo -n \|
 		if [ ! -e $f.out ] ;then
-			NUM_FAIL=`expr $NUM_FAIL + 1`
-			echo $f.out missing.
+			NUM_FAIL="$NUM_FAIL + 1"
+			ERRORS="$ERRORS\n$f.out missing."
 		else
 			if ($f 2>&1 | diff -q $f.out - > /dev/null) ;then
-				NUM_PASS=`expr $NUM_PASS + 1`
+				NUM_PASS="$NUM_PASS + 1"
 			else
-				NUM_FAIL=`expr $NUM_FAIL + 1`
-				echo $f Failed.
+				NUM_FAIL="$NUM_FAIL + 1"
+				ERRORS="$ERRORS\n$f Failed."
 			fi
 		fi
 	fi
 done
+
+echo -e $ERRORS
+NUM_PASS=`expr $NUM_PASS`
+NUM_FAIL=`expr $NUM_FAIL`
 echo Passed: $NUM_PASS
 echo Failed: $NUM_FAIL
 exit $NUM_FAIL
